3ddae7395bd77fe83730fc068fcb602dac7b22d4
Wildan Rasiono <wildanrasiono@gmail.com>
2025-05-22 14:47:23 +0700

    update wildan

---
+++ admin/layout/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="footer">
+	<p>&copy; fajar & gilang </p>
+	<p>projek pkl smkn 1 semparuk</p>
+</footer>
---
+++ admin/layout/navbar.php
@@ -0,0 +1,3 @@
+<nav class="navbar1">
+	<i class='bx bx-menu'></i>
+</nav>
